14c68cf0c2c01461469150af67cabf5a9e502fb0
John Gribbin <johnggribbin@gmail.com>
2019-03-18 13:12:50 -0400

    add color constants from designs

---
+++ client/constants/index.js
@@ -0,0 +1,9 @@
+export const colors = {
+  black: "#333333",
+  blue: "#61E3FF",
+  grey: "#454D58",
+  mainBackground: "#f7f5ee",
+  orange: "#FF5730",
+  pink: "#FF5C83",
+  tertiaryOrange: "#F9A549"
+};
