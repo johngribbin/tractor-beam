0a500ee9364012cd88385dd85248290013a51c72
John Gribbin <johnggribbin@gmail.com>
2019-03-19 16:58:13 -0400

    Add ClaimGift component to wrap claim gift button and sign up form

---
+++ client/components/ClaimGift.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
+
+import GiftIcon from "../components/GiftIcon";
+import MainButton from "../components/MainButton";
+import SignUpForm from "../components/SignUpForm";
+
+class ClaimGift extends Component {
+  state = {
+    showGiftIcon: true,
+    showButton: true,
+    showForm: false
+  };
+
+  _claimGift = () => {
+    // if user is not logged in
+    if (!this.props.isLoggedIn) {
+      this._showForm();
+    }
+    // if user is logged in
+    else {
+      console.log("you are logged in!");
+    }
+  };
+
+  _showForm = () => {
+    this.setState({
+      showGiftIcon: false,
+      showButton: false,
+      showForm: true
+    });
+  };
+
+  _hideForm = () => {
+    this.setState({
+      showGiftIcon: true,
+      showButton: true,
+      showForm: false
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.claimGiftContainer}>
+        {this.state.showGiftIcon ? (
+          <GiftIcon giftMessage={"Someone sent you a gift!"} />
+        ) : null}
+
+        {this.state.showButton ? (
+          <MainButton title={"CLAIM GIFT!"} onPress={this._claimGift} />
+        ) : null}
+        {this.state.showForm ? <SignUpForm hideForm={this._hideForm} /> : null}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  claimGiftContainer: {
+    marginTop: 200
+  }
+});
+
+// Add this function to every component
+const mapStateToProps = state => {
+  console.log(state);
+
+  return {
+    // key name should match name of key for the reducer in combineReducers function in /reducer/index
+    isLoggedIn: state.isLoggedIn
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logIn: () => {
+      dispatch(logIn());
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ClaimGift);
---
+++ client/components/SignUpForm.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { colors } from "../constants";
+import { View, Text, TextInput } from "react-native";
+
+import MainButton from "../components/MainButton";
+
+import { addEmail, logIn } from "../redux/actions";
+
+class SignUpForm extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  _submitForm = () => {
+    // if user has provided email and password, log them in and hide the form
+    if (this.state.email && this.state.password) {
+      this.props.addEmail([
+        {
+          address: this.state.email,
+          default: true
+        }
+      ]);
+      this.props.logIn();
+      this.props.hideForm();
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.formWrapper}>
+        <Text style={styles.header}>Sign UP</Text>
+        <Text style={styles.subHeader}>
+          Submit an email and password to sign up and claim your gifts
+        </Text>
+
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="example@example.com"
+          onChangeText={email => this.setState({ email })}
+        />
+
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          type="password"
+          style={styles.textInput}
+          placeholder="password"
+          onChangeText={password => this.setState({ password })}
+        />
+
+        <MainButton
+          style={this.state.email && this.state.password ? {} : styles.button}
+          title={"SIGN UP"}
+          onPress={this._submitForm}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = {
+  formWrapper: {
+    padding: 5,
+    width: "75%"
+  },
+  header: {},
+  subHeader: {},
+  label: {},
+  textInput: {
+    backgroundColor: "white",
+    borderColor: colors.lightGrey,
+    borderRadius: 4,
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingVertical: 2
+  },
+  button: {
+    opacity: 0.5
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addEmail: email => {
+      dispatch(addEmail(email));
+    },
+    logIn: () => {
+      dispatch(logIn());
+    }
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignUpForm);
