267834b28201f57d87a1677ad6f864eae70d79be
John Gribbin <johnggribbin@gmail.com>
2019-03-19 16:55:15 -0400

    Add lightGrey and darkGrey colors

--- client/constants/index.js
+++ client/constants/index.js
@@ -1,7 +1,8 @@
 export const colors = {
   black: "#333333",
   blue: "#61E3FF",
-  grey: "#454D58",
+  lightGrey: "#B6B6B6",
+  darkGrey: "#454D58",
   mainBackground: "#f7f5ee",
   orange: "#FF5730",
   pink: "#FF5C83",
