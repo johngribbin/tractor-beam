4c759dbb8142b3c4004c3f42c9340eeb036b8f41
John Gribbin <johnggribbin@gmail.com>
2019-03-20 16:53:01 -0400

    Create LinkExternalAccount component and add to AccountScreen component

---
+++ client/components/AltButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { colors } from "../constants";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+
+export default function MainButton(props) {
+  return (
+    <View>
+      <TouchableHighlight
+        style={
+          props.style ? { ...styles.button, ...props.style } : styles.button
+        }
+        onPress={props.onPress}
+      >
+        <Text style={{ ...styles.buttonText }}>{props.title}</Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 5,
+    borderColor: colors.orange,
+    borderWidth: 5,
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    shadowColor: colors.lightGrey,
+    shadowOffset: { height: 0, width: 0 },
+    padding: 10
+  },
+  buttonText: {
+    fontFamily: "barlow-bold",
+    fontSize: 14,
+    fontWeight: "600",
+    lineHeight: 15,
+    color: colors.orange,
+    textAlign: "center"
+  }
+});
---
+++ client/components/LinkExternalAccount.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+import AltButton from "../components/AltButton";
+
+import { colors, smallText } from "../constants";
+
+import { connect } from "react-redux";
+
+function LinkExternalAccount(props) {
+  const { externalAccounts } = props;
+
+  return (
+    <View style={styles.linkExternalAccountContainer}>
+      <Image
+        style={styles.image}
+        source={require("../assets/images/LinkSymbol.png")}
+      />
+      <AltButton title={"LINK EXTERNAL ACCOUNT"} />
+      {externalAccounts.length === 0 ? (
+        <Text style={{ ...smallText, ...styles.message }}>
+          you haven't linked an external account yet
+        </Text>
+      ) : null}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  linkExternalAccountContainer: {
+    alignItems: "center",
+    display: "flex"
+  },
+  image: {
+    marginBottom: 10
+  },
+  message: {
+    color: colors.orange,
+    marginTop: 10
+  }
+});
+
+const mapStateToProps = state => {
+  return {
+    externalAccounts: state.externalAccounts
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(LinkExternalAccount);
