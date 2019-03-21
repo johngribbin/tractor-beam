8607039bd0e20dca0232a25f122846a78f5a6572
John Gribbin <johnggribbin@gmail.com>
2019-03-21 17:21:17 -0400

    Require SecuritySymbol in _loadResourcesAsync call

--- client/App.js
+++ client/App.js
@@ -18,7 +18,8 @@ export default class App extends React.Component {
         //require("./assets/images/robot-dev.png"),
         //require("./assets/images/robot-prod.png")
         require("./assets/images/GiftBlack.png"),
-        require("./assets/images/LinkSymbol.png")
+        require("./assets/images/LinkSymbol.png"),
+        require("./assets/images/SecuritySymbol.png")
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
