86dff3db9e0ec27b57f70228d9a628181f471160
John Gribbin <johnggribbin@gmail.com>
2019-03-22 17:30:01 -0400

    Finish method which renders current external accounts and style accordingly

--- client/components/ExternalAccounts.js
+++ client/components/ExternalAccounts.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+
+import { colors, largeText } from "../constants";
 
 import { connect } from "react-redux";
 
@@ -8,14 +10,25 @@ function ExternalAccounts(props) {
     const { externalAccounts } = props;
 
     return externalAccounts.map(externalAccount => (
-      <View key={externalAccount.address}>
-        <Text>{externalAccount.name}</Text>
-        <Text>{externalAccount.address}</Text>
+      <View style={styles.accountContainer} key={externalAccount.address}>
+        <Text style={{ ...largeText, ...styles.text }}>
+          {externalAccount.name}
+        </Text>
+        {externalAccount.default ? (
+          <Text style={styles.text}>default account</Text>
+        ) : null}
+        <View style={styles.textContainer}>
+          <Text>{externalAccount.address}</Text>
+        </View>
       </View>
     ));
   };
 
-  return <View>{this._renderExternalAccounts()}</View>;
+  return (
+    <View style={styles.componentContainer}>
+      {this._renderExternalAccounts()}
+    </View>
+  );
 }
 
 const mapStateToProps = state => {
@@ -23,6 +36,38 @@ const mapStateToProps = state => {
     externalAccounts: state.externalAccounts
   };
 };
+
+const styles = StyleSheet.create({
+  componentContainer: {
+    alignItems: "center",
+    borderBottomColor: colors.lightGrey,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    paddingBottom: 10,
+    display: "flex",
+    flex: 1
+  },
+  accountContainer: {
+    backgroundColor: colors.darkGrey,
+    borderRadius: 5,
+    display: "flex",
+    marginBottom: 10,
+    padding: 5,
+    width: "100%"
+  },
+  textContainer: {
+    backgroundColor: "white",
+    borderColor: colors.lightGrey,
+    borderRadius: 4,
+    borderWidth: 1,
+    padding: 5,
+    width: "95%"
+  },
+  text: {
+    color: "white",
+    display: "flex"
+  }
+});
 
 export default connect(
   mapStateToProps,
