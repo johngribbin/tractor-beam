85b0c6d0a421e1ab58763e7cf8f72b4a5a7b48fe
John Gribbin <johnggribbin@gmail.com>
2019-03-25 12:31:51 -0400

    Reinstall expo on android emulator

--- client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -23,9 +23,12 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@john.gribbin/client", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40john.gribbin%2Fclient%2F1.0.0%2Fe9da27045a3d38ad7d3c7f9ee449d957-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
