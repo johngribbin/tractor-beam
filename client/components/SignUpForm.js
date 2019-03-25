3ca92e9abba23e25dad3f1009b3e7ca3462b1c1e
John Gribbin <johnggribbin@gmail.com>
2019-03-25 17:17:33 -0400

    Update styling

--- client/components/SignUpForm.js
+++ client/components/SignUpForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { colors } from "../constants";
+import { colors, largeText, mediumTextBold, mediumText } from "../constants";
 import { View, Text, TextInput } from "react-native";
 
 import MainButton from "../components/MainButton";
@@ -33,44 +33,53 @@ class SignUpForm extends Component {
 
   render() {
     return (
-      <View style={styles.formWrapper}>
-        <Text style={styles.header}>Sign UP</Text>
-        <Text style={styles.subHeader}>
+      <View style={styles.componentContainer}>
+        <Text style={{ ...largeText, ...styles.header }}>Sign Up</Text>
+        <Text style={{ ...mediumText, ...styles.subHeader }}>
           Submit an email and password to sign up and claim your gifts
         </Text>
+        <View style={styles.formContainer}>
+          <Text style={{ ...mediumTextBold, ...styles.label }}>Email</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="example@example.com"
+            onChangeText={email => this.setState({ email })}
+          />
 
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="example@example.com"
-          onChangeText={email => this.setState({ email })}
-        />
+          <Text style={{ ...mediumTextBold, ...styles.label }}>Password</Text>
+          <TextInput
+            type="password"
+            style={styles.textInput}
+            placeholder="password"
+            onChangeText={password => this.setState({ password })}
+          />
 
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          type="password"
-          style={styles.textInput}
-          placeholder="password"
-          onChangeText={password => this.setState({ password })}
-        />
-
-        <MainButton
-          style={this.state.email && this.state.password ? {} : styles.button}
-          title={"SIGN UP"}
-          onPress={this._submitForm}
-        />
+          <MainButton
+            style={this.state.email && this.state.password ? {} : styles.button}
+            title={"SIGN UP"}
+            onPress={this._submitForm}
+          />
+        </View>
       </View>
     );
   }
 }
 
 const styles = {
-  formWrapper: {
-    padding: 5,
-    width: "75%"
+  componentContainer: {
+    display: "flex"
   },
-  header: {},
-  subHeader: {},
+  formContainer: {
+    alignSelf: "center",
+    marginTop: 10,
+    width: "90%"
+  },
+  header: {
+    marginBottom: 10
+  },
+  subHeader: {
+    marginBottom: 10
+  },
   label: {},
   textInput: {
     backgroundColor: "white",
@@ -78,9 +87,10 @@ const styles = {
     borderRadius: 4,
     borderWidth: 1,
     marginBottom: 10,
-    paddingVertical: 2
+    padding: 7.5
   },
   button: {
+    marginTop: 10,
     opacity: 0.5
   }
 };
