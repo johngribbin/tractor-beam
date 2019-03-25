ab40e2d1468c55575b1e969bad5ada1d42448202
John Gribbin <johnggribbin@gmail.com>
2019-03-25 12:34:27 -0400

    Add more text constants

--- client/constants/index.js
+++ client/constants/index.js
@@ -10,7 +10,21 @@ export const colors = {
 };
 
 export const smallText = {
+  fontFamily: "barlow-regular",
+  fontSize: 12,
+  fontWeight: "300",
+  lineHeight: 12
+};
+
+export const smallTextItalic = {
   fontFamily: "barlow-italic",
+  fontSize: 12,
+  fontWeight: "300",
+  lineHeight: 12
+};
+
+export const smallTextBold = {
+  fontFamily: "barlow-bold",
   fontSize: 12,
   fontWeight: "300",
   lineHeight: 12
@@ -18,6 +32,12 @@ export const smallText = {
 
 export const mediumText = {
   fontFamily: "barlow-regular",
+  fontSize: 18,
+  lineHeight: 20
+};
+
+export const mediumTextItalic = {
+  fontFamily: "barlow-italic",
   fontSize: 18,
   lineHeight: 20
 };
@@ -30,6 +50,12 @@ export const mediumTextBold = {
 
 export const largeText = {
   fontFamily: "barlow-regular",
+  fontSize: 24,
+  lineHeight: 24
+};
+
+export const largeTextItalic = {
+  fontFamily: "barlow-italic",
   fontSize: 24,
   lineHeight: 24
 };
