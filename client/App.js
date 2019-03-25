eccfc73cba48a3f023a0cefc04ff910f9450f309
John Gribbin <johnggribbin@gmail.com>
2019-03-25 17:14:30 -0400

    Add require function for new images added

--- client/App.js
+++ client/App.js
@@ -19,7 +19,9 @@ export default class App extends React.Component {
         //require("./assets/images/robot-prod.png")
         require("./assets/images/GiftBlack.png"),
         require("./assets/images/LinkSymbol.png"),
-        require("./assets/images/SecuritySymbol.png")
+        require("./assets/images/SecuritySymbol.png"),
+        require("./assets/images/DownArrow.png"),
+        require("./assets/images/UpArrow.png")
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
