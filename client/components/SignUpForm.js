bd649319f2c4b6931760047a4b2682cd0895bf25
John Gribbin <johnggribbin@gmail.com>
2019-03-27 17:39:20 -0400

    Introduce Formik for form validation

--- client/components/SignUpForm.js
+++ client/components/SignUpForm.js
@@ -1,86 +1,96 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
-import { colors, largeText, mediumTextBold, mediumText } from "../constants";
+import { colors, mediumTextBold } from "../constants";
 import { View, Text, TextInput } from "react-native";
-
 import MainButton from "../components/MainButton";
-
 import { addEmail, logIn } from "../redux/actions";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 class SignUpForm extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
-
-  _submitForm = () => {
-    // when user has provided email and password, log them in and hide the form
-    if (this.state.email && this.state.password) {
-      // add email to app state and set to default email
-      this.props.addEmail([
-        {
-          address: this.state.email,
-          default: true
-        }
-      ]);
-      // switch isLoggedIn state property to true
-      this.props.logIn();
-      // hide the form component from ClaimGift component
-      this.props.hideForm();
-    }
-  };
-
   render() {
     return (
-      <View style={styles.componentContainer}>
-        <Text style={{ ...largeText, ...styles.header }}>Sign Up</Text>
-        <Text style={{ ...mediumText, ...styles.subHeader }}>
-          Submit an email and password to sign up and claim your gifts
-        </Text>
-        <View style={styles.formContainer}>
-          <Text style={{ ...mediumTextBold, ...styles.label }}>Email</Text>
-          <TextInput
-            style={styles.textInput}
-            placeholder="example@example.com"
-            onChangeText={email => this.setState({ email })}
-          />
-
-          <Text style={{ ...mediumTextBold, ...styles.label }}>Password</Text>
-          <TextInput
-            type="password"
-            style={styles.textInput}
-            placeholder="password"
-            onChangeText={password => this.setState({ password })}
-          />
-
-          <MainButton
-            style={this.state.email && this.state.password ? {} : styles.button}
-            title={"SIGN UP"}
-            onPress={this._submitForm}
-          />
-        </View>
-      </View>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={values => {
+          // add the email to state object
+          this.props.addEmail([
+            {
+              address: values.email,
+              default: true
+            }
+          ]);
+          // switch isLoggedIn state property to true
+          this.props.logIn();
+          // navigate user back to home
+          this.props.navigate("Home");
+        }}
+        validationSchema={yup.object().shape({
+          email: yup
+            .string()
+            .email()
+            .required(),
+          password: yup
+            .string()
+            .min(6)
+            .required()
+        })}
+      >
+        {({
+          values,
+          handleChange,
+          errors,
+          setFieldTouched,
+          touched,
+          isValid,
+          handleSubmit
+        }) => (
+          <View style={styles.formContainer}>
+            <Text style={{ ...mediumTextBold, ...styles.label }}>Email</Text>
+            <TextInput
+              style={styles.textInput}
+              value={values.email}
+              onChangeText={handleChange("email")}
+              onBlur={() => setFieldTouched("email")}
+              placeholder="E-mail"
+            />
+            {touched.email && errors.email && (
+              <Text style={{ fontSize: 10, color: "red" }}>{errors.email}</Text>
+            )}
+            <Text style={{ ...mediumTextBold, ...styles.label }}>Password</Text>
+            <TextInput
+              style={styles.textInput}
+              value={values.password}
+              onChangeText={handleChange("password")}
+              placeholder="Password"
+              onBlur={() => setFieldTouched("password")}
+              secureTextEntry={true}
+            />
+            {touched.password && errors.password && (
+              <Text style={{ fontSize: 10, color: "red" }}>
+                {errors.password}
+              </Text>
+            )}
+            <MainButton
+              style={styles.button}
+              title="SIGN UP"
+              disabled={!isValid}
+              onPress={() => handleSubmit()}
+            />
+          </View>
+        )}
+      </Formik>
     );
   }
 }
 
 const styles = {
-  componentContainer: {
-    display: "flex"
-  },
   formContainer: {
     alignSelf: "center",
+    flex: 1,
     marginTop: 10,
     width: "90%"
   },
-  header: {
-    marginBottom: 10
-  },
-  subHeader: {
-    marginBottom: 10
-  },
-  label: {},
   textInput: {
     backgroundColor: "white",
     borderColor: colors.lightGrey,
@@ -90,8 +100,7 @@ const styles = {
     padding: 7.5
   },
   button: {
-    marginTop: 10,
-    opacity: 0.5
+    marginTop: 10
   }
 };
 
