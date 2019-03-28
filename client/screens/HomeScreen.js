0136fb9f011dc70a817477d1c2019a06dd819fa1
John Gribbin <johnggribbin@gmail.com>
2019-03-28 14:23:42 -0400

    Remove unused code from boilerplate and add log for state object

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 import { colors } from "../constants/";
 
@@ -19,8 +18,6 @@ import {
 import { WebBrowser } from "expo";
 
 import { MonoText } from "../components/StyledText";
-//const bip39 = require("react-native-bip39");
-//const hdKey = require("ethereumjs-wallet/hdkey");
 
 import "ethers/dist/shims.js";
 import { Wallet } from "ethers";
@@ -45,7 +42,7 @@ class HomeScreen extends Component {
     title: "Home"
   };
 
-  async componentDidMount() {
+  _createAccount = async () => {
     let mnemonic = "";
 
     try {
@@ -62,25 +59,47 @@ class HomeScreen extends Component {
       console.error(error);
     }
 
-    // permissionedAccountsReducer tests
-    this.props.addPermissionedAccount([
-      {
-        recoveryPhrase: mnemonic,
-        address: "0x555",
-        balance: 0,
-        linkedContract: "0x321",
-        default: true,
-        revealedRecoveryPhrase: false
-      }
-    ]);
+    const account = await Wallet.fromMnemonic(mnemonic);
+    return account;
+  };
 
-    // contractAccountReducer tests - need to addPermissionedAddress action creator!!
-    this.props.setContractAccount({
-      address: "0x321",
-      balance: 0,
-      permissionedAddresses: ["0x123"],
-      revealedAddress: false
-    });
+  async componentDidMount() {
+    // only ran when home page mounts for first time
+    if (this.props.permissionedAccounts.length === 0) {
+      const permissionedAccount = await this._createAccount();
+      const contractAccount = await this._createAccount();
+
+      // permissionedAccountsReducer tests
+      this.props.addPermissionedAccount([
+        {
+          recoveryPhrase: permissionedAccount.mnemonic,
+          address: permissionedAccount.address,
+          balance: 0,
+          linkedContract: contractAccount.address,
+          default: true,
+          revealedRecoveryPhrase: false
+        }
+      ]);
+
+      // contractAccountReducer tests - need to addPermissionedAddress action creator!!
+      this.props.setContractAccount({
+        address: contractAccount.address,
+        balance: 0,
+        permissionedAddresses: [permissionedAccount.address],
+        revealedAddress: false
+      });
+
+      // emailReducer tests
+      this.props.addEmail([
+        {
+          address: "bob@cryptonoob.com",
+          default: true
+        }
+      ]);
+
+      //isLoggedInReducer tests
+      this.props.logIn();
+    }
 
     /*
     // externalAccountsReducer
@@ -123,11 +142,7 @@ class HomeScreen extends Component {
     this.props.setDefaultEmail("bob@cryptoexpert.com");
     this.props.deleteEmail("bob@gmail.com");
     
-    // mnemonic creatior tests
-    const mnemonicWallet = Wallet.fromMnemonic(mnemonic);
-    console.log(`mnemonic is ${mnemonicWallet.mnemonic}`);
-    console.log(`private key is ${mnemonicWallet.privateKey}`);
-    console.log(`address is ${mnemonicWallet.address}`);
+    
     
     // externalAccountsReducer tests
     this.props.addExternalAccount([
@@ -148,96 +163,14 @@ class HomeScreen extends Component {
     */
   }
 
-  /*
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync("https://google.com");
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      "https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes"
-    );
-  };
-  */
-
   render() {
     const { navigate } = this.props.navigation;
 
     return (
       <View style={styles.homeContainer}>
         <ScrollView contentContainerStyle={styles.contentContainer}>
-          {/* Renders the signup/signin form if the users is not logged in*/}
           <ClaimGift navigate={navigate} />
-
-          {/*
-            <View style={styles.getStartedContainer}>
-              {this._maybeRenderDevelopmentModeWarning()}
-  
-              <Text style={styles.getStartedText}>Get started by opening</Text>
-  
-              <View
-                style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-              >
-                <MonoText style={styles.codeHighlightText}>
-                  screens/HomeScreen.js
-                </MonoText>
-              </View>
-  
-              <Text style={styles.getStartedText}>Hello World!</Text>
-            </View>
-  
-            <View style={styles.helpContainer}>
-              <TouchableOpacity
-                onPress={this._handleHelpPress}
-                style={styles.helpLink}
-              >
-                <Text style={styles.helpLinkText}>
-                  Help, it didn’t automatically reload!
-                </Text>
-              </TouchableOpacity>
-            </View>
-            */}
         </ScrollView>
-
-        {/*}
-          <View style={styles.tabBarInfoContainer}>
-            <Text style={styles.tabBarInfoText}>
-              This is a tab bar. You can edit it in:
-            </Text>
-  
-            <View
-              style={[styles.codeHighlightContainer, styles.navigationFilename]}
-            >
-              <MonoText style={styles.codeHighlightText}>
-                navigation/MainTabNavigator.js
-              </MonoText>
-            </View>
-            
-          </View>  
-          */}
       </View>
     );
   }
@@ -253,94 +186,16 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     paddingTop: 200
-    /*
-    borderColor: "red",
-    borderStyle: "solid",
-    borderWidth: 1
-    */
   }
-  /*
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center"
-  },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10
-  },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
-  },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center"
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
-  }
-  */
 });
+
+const mapStateToProps = state => {
+  console.log(state);
+
+  return {
+    permissionedAccounts: state.permissionedAccounts
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -378,6 +233,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(HomeScreen);
