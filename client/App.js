70313112b126ea1c024d61be0b7c8531f59012e4
John Gribbin <johnggribbin@gmail.com>
2019-04-01 17:48:29 -0400

    Configure redux to include persistor from redux-persist

--- client/App.js
+++ client/App.js
@@ -2,23 +2,22 @@ import React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { AppLoading, Asset, Font, Icon } from "expo";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import ReduxThunk from "redux-thunk";
-
+import { PersistGate } from "redux-persist/integration/react";
+import configureStore from "./redux/configureStore";
 import AppNavigator from "./navigation/AppNavigator";
-import reducers from "./redux/reducers";
-const store = createStore(reducers, applyMiddleware(ReduxThunk));
+
+const { persistor, store } = configureStore();
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false
   };
 
+  componentDidMount() {}
+
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        //require("./assets/images/robot-dev.png"),
-        //require("./assets/images/robot-prod.png")
         require("./assets/images/GiftBlack.png"),
         require("./assets/images/LinkSymbol.png"),
         require("./assets/images/SecuritySymbol.png"),
@@ -28,9 +27,7 @@ export default class App extends React.Component {
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
+
         "barlow-regular": require("./assets/fonts/Barlow-Regular.ttf"),
         "barlow-bold": require("./assets/fonts/Barlow-Bold.ttf"),
         "barlow-italic": require("./assets/fonts/Barlow-Italic.ttf")
@@ -62,7 +59,9 @@ export default class App extends React.Component {
         <View style={styles.container}>
           {Platform.OS === "ios" && <StatusBar barStyle="default" />}
           <Provider store={store}>
-            <AppNavigator />
+            <PersistGate loading={null} persistor={persistor}>
+              <AppNavigator />
+            </PersistGate>
           </Provider>
         </View>
       );
