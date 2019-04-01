7898d9ad959036fe765e3c23995dc955848e2ba5
John Gribbin <johnggribbin@gmail.com>
2019-04-01 12:05:35 -0400

    Remove header settings

--- client/screens/SettingsScreen.js
+++ client/screens/SettingsScreen.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+import { ExpoConfigView } from "@expo/samples";
 
 export default class SettingsScreen extends React.Component {
+  /*
   static navigationOptions = {
     title: 'app.json',
   };
+  */
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
